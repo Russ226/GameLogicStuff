070a9cd2a3d18263b0a59f71664fb6e8966406ae
Ruslan Radetskiy <russ2510@gmail.com>
2019-05-04 20:09:15 -0400

    Set up proj

---
+++ Main/Runner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameTick.Main
+{
+    public class Runner
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("check one two");
+        }
+    }
+}
